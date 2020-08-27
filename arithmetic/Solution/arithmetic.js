27242a7ce46ee21bcacb7fe3fca8ebdb9eaceea7
Nicole Graiff <nicole.graiff@gmail.com>
2020-08-27 10:20:51 -0400

    arithmetic algo practice

---
+++ arithmetic/Solution/arithmetic.js
@@ -0,0 +1,22 @@
+// Fill in the body of each function with the code required
+let result = 0;
+
+const add = (num1, num2) => {
+    result = num1 + num2;
+    return result;
+};
+
+const subtract = (num1, num2) => {
+    result = num1 - num2;
+    return result;
+};
+
+const multiply = (num1, num2) => {
+    result = num1 * num2;
+    return result;
+};
+
+const divide = (num1, num2) => {
+    result = num1 / num2;
+    return result;
+};
